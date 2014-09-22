693e89d9118a794a998d595bd74e9a7fd332f80a
SebasMS <smejias1996@gmail.com>
2014-09-21 20:55:32 -0500

    Project

---
+++ js/animacionBoton.js
@@ -0,0 +1,18 @@
+/* Función que ejecuta cuando el documento o página halla cargado*/
+$(document).ready(function(){
+
+	$('.go-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 0) {
+			$('.go-up').slideDown(300);
+		}else{
+			$('.go-up').slideUp(300);
+		}
+	});
+
+});
